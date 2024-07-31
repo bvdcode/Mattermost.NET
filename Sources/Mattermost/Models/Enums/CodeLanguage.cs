e256b25f9ee71e620b93a5e9e9a8b60fca48ed26
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-07-30 20:31:44 -0700

    Refactor MarkdownHelpers to use CodeLanguage enum
    Updated using directive in MarkdownHelpers.cs to include Mattermost.Models.Enums. Modified CodeBlock method to use CodeLanguage enum instead of a string for the language parameter, with a default value of CodeLanguage.Text. Added CodeLanguage.cs defining the CodeLanguage enum with various programming languages.

---
+++ Sources/Mattermost/Models/Enums/CodeLanguage.cs
@@ -0,0 +1,323 @@
+﻿namespace Mattermost.Models.Enums
+{
+    /// <summary>
+    /// Markdown code language.
+    /// </summary>
+    public enum CodeLanguage
+    {
+        /// <summary>
+        /// actionscript, as, as3
+        /// </summary>
+        ActionScript = 1,
+
+        /// <summary>
+        /// applescript
+        /// </summary>
+        AppleScript = 2,
+
+        /// <summary>
+        /// bash, sh
+        /// </summary>
+        Bash = 3,
+
+        /// <summary>
+        /// clojure
+        /// </summary>
+        Clojure = 4,
+
+        /// <summary>
+        /// coffescript, coffee, coffee-script
+        /// </summary>
+        CoffeeScript = 5,
+
+        /// <summary>
+        /// cpp, c++, c
+        /// </summary>
+        Cpp = 6,
+
+        /// <summary>
+        /// cs, c#, csharp
+        /// </summary>
+        CSharp = 7,
+
+        /// <summary>
+        /// css
+        /// </summary>
+        CSS = 8,
+
+        /// <summary>
+        /// d, dlang
+        /// </summary>
+        DLang = 9,
+
+        /// <summary>
+        /// dart
+        /// </summary>
+        Dart = 10,
+
+        /// <summary>
+        /// delphi
+        /// </summary>
+        Delphi = 11,
+
+        /// <summary>
+        /// diff, patch, udiff
+        /// </summary>
+        Diff = 12,
+
+        /// <summary>
+        /// django
+        /// </summary>
+        Django = 13,
+
+        /// <summary>
+        /// dockerfile, docker
+        /// </summary>
+        Dockerfile = 14,
+
+        /// <summary>
+        /// elixir, ex, exs
+        /// </summary>
+        Elixir = 15,
+
+        /// <summary>
+        /// erlang, erl
+        /// </summary>
+        Erlang = 16,
+
+        /// <summary>
+        /// fortran
+        /// </summary>
+        Fortran = 17,
+
+        /// <summary>
+        /// fsharp
+        /// </summary>
+        FSharp = 18,
+
+        /// <summary>
+        /// gcode
+        /// </summary>
+        GCode = 19,
+
+        /// <summary>
+        /// go, golang
+        /// </summary>
+        Golang = 20,
+
+        /// <summary>
+        /// groovy
+        /// </summary>
+        Groovy = 21,
+
+        /// <summary>
+        /// handlebars, hbs, mustache
+        /// </summary>
+        Handlebars = 22,
+
+        /// <summary>
+        /// haskell, hs
+        /// </summary>
+        Haskell = 23,
+
+        /// <summary>
+        /// haxe
+        /// </summary>
+        Haxe = 24,
+
+        /// <summary>
+        /// java
+        /// </summary>
+        Java = 25,
+
+        /// <summary>
+        /// javascript, js
+        /// </summary>
+        JavaScript = 26,
+
+        /// <summary>
+        /// json
+        /// </summary>
+        JSON = 27,
+
+        /// <summary>
+        /// julia, jl
+        /// </summary>
+        Julia = 28,
+
+        /// <summary>
+        /// kotlin
+        /// </summary>
+        Kotlin = 29,
+
+        /// <summary>
+        /// latex, tex
+        /// </summary>
+        LaTeX = 30,
+
+        /// <summary>
+        /// less
+        /// </summary>
+        Less = 31,
+
+        /// <summary>
+        /// lisp
+        /// </summary>
+        Lisp = 32,
+
+        /// <summary>
+        /// lua
+        /// </summary>
+        Lua = 33,
+
+        /// <summary>
+        /// makefile, make, mf, gnumake, bsdmake
+        /// </summary>
+        Makefile = 34,
+
+        /// <summary>
+        /// markdown, md, mkd
+        /// </summary>
+        Markdown = 35,
+
+        /// <summary>
+        /// matlab, m
+        /// </summary>
+        Matlab = 36,
+
+        /// <summary>
+        /// objectivec, objective_c, objc
+        /// </summary>
+        ObjectiveC = 37,
+
+        /// <summary>
+        /// ocaml
+        /// </summary>
+        OCaml = 38,
+
+        /// <summary>
+        /// perl, pl
+        /// </summary>
+        Perl = 39,
+
+        /// <summary>
+        /// pascal, pas
+        /// </summary>
+        Pascal = 40,
+
+        /// <summary>
+        /// pgsql, postgres, postgresql
+        /// </summary>
+        PostgreSQL = 41,
+
+        /// <summary>
+        /// php, php3, php4, php5
+        /// </summary>
+        PHP = 42,
+
+        /// <summary>
+        /// powershell, posh
+        /// </summary>
+        PowerShell = 43,
+
+        /// <summary>
+        /// puppet, pp
+        /// </summary>
+        Puppet = 44,
+
+        /// <summary>
+        /// python, py
+        /// </summary>
+        Python = 45,
+
+        /// <summary>
+        /// r, s
+        /// </summary>
+        R = 46,
+
+        /// <summary>
+        /// ruby, rb
+        /// </summary>
+        Ruby = 47,
+
+        /// <summary>
+        /// rust, rs
+        /// </summary>
+        Rust = 48,
+
+        /// <summary>
+        /// scala
+        /// </summary>
+        Scala = 49,
+
+        /// <summary>
+        /// scheme
+        /// </summary>
+        Scheme = 50,
+
+        /// <summary>
+        /// scss
+        /// </summary>
+        SCSS = 51,
+
+        /// <summary>
+        /// smalltalk, st, squeak
+        /// </summary>
+        Smalltalk = 52,
+
+        /// <summary>
+        /// sql
+        /// </summary>
+        SQL = 53,
+
+        /// <summary>
+        /// stylus, styl
+        /// </summary>
+        Stylus = 54,
+
+        /// <summary>
+        /// swift
+        /// </summary>
+        Swift = 55,
+
+        /// <summary>
+        /// text
+        /// </summary>
+        Text = 56,
+
+        /// <summary>
+        /// typescript, ts, tsx
+        /// </summary>
+        TypeScript = 57,
+
+        /// <summary>
+        /// vbnet, vb, visualbasic
+        /// </summary>
+        VBNet = 58,
+
+        /// <summary>
+        /// vbscript
+        /// </summary>
+        VBScript = 59,
+
+        /// <summary>
+        /// verilog
+        /// </summary>
+        Verilog = 60,
+
+        /// <summary>
+        /// vhdl
+        /// </summary>
+        VHDL = 61,
+
+        /// <summary>
+        /// html, xml
+        /// </summary>
+        HTML = 62,
+
+        /// <summary>
+        /// yaml, yml
+        /// </summary>
+        YAML = 63
+    }
+}
