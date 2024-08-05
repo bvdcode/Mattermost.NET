441ee1a505c196a669a6e1bdff7dfa4b565f9ae5
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-08-04 17:27:52 -0700

    Add async Main and event handlers in console test project
    Updated Mattermost.ConsoleTest.csproj to reference Mattermost.csproj.
    Changed Main method in Program.cs to async Task Main.
    Initialized MattermostClient with server URL and token.
    Added event handlers for OnLogMessage and OnMessageReceived.
    Client_OnMessageReceived logs non-empty messages.
    Client_OnLogMessage logs messages with a timestamp.

--- Sources/Mattermost.ConsoleTest/Program.cs
+++ Sources/Mattermost.ConsoleTest/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Mattermost.ConsoleTest
+﻿using Mattermost.Events;
+
+namespace Mattermost.ConsoleTest
 {
     public class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
+            const string token = "37VlFKySIZn6gryA85cR1GKBQkjmfRZ6";
+            const string server = "https://mm.your-server.com";
+            MattermostClient client = new(server, token);
+            client.OnLogMessage += Client_OnLogMessage;
+            client.OnMessageReceived += Client_OnMessageReceived;
+            await client.StartReceivingAsync();
+        }
 
+        private static void Client_OnMessageReceived(object? sender, MessageEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(e.Message.Post.Text))
+            {
+                return;
+            }
+            LogEventArgs args = new(e.Message.Post.Text);
+            Client_OnLogMessage(sender, args);
+        }
+
+        private static void Client_OnLogMessage(object? sender, LogEventArgs e)
+        {
+            Console.WriteLine("[{0}]: {1}", DateTime.Now, e.Message);
         }
     }
 }
