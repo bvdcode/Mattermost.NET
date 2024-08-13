2068da2514da7823c35547fdd3aa2763466d166f
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-08-13 02:23:19 -0700

    Refactor exception classes and add XML documentation
    Introduce XML documentation comments for `ApiKeyException`,
    `AuthorizationException`, and `MattermostClientException` to
    explain their purposes. Change constructors from `public` to
    `internal` to restrict their accessibility within the same
    assembly. Replace empty constructor bodies with single-line
    constructors for improved readability and conciseness.

--- Sources/Mattermost/Exceptions/AuthorizationException.cs
+++ Sources/Mattermost/Exceptions/AuthorizationException.cs
@@ -2,18 +2,15 @@
 
 namespace Mattermost.Exceptions
 {
+    /// <summary>
+    /// Thrown when credentials are invalid or the user is not authorized to perform the requested operation.
+    /// </summary>
     public class AuthorizationException : MattermostClientException
     {
-        public AuthorizationException()
-        {
-        }
+        internal AuthorizationException() { }
 
-        public AuthorizationException(string message) : base(message)
-        {
-        }
+        internal AuthorizationException(string message) : base(message) { }
 
-        public AuthorizationException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
+        internal AuthorizationException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
