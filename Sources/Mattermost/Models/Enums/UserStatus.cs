ed48cadbce121407660c8ffc44d2c57bbfa8327d
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2024-08-13 02:12:52 -0700

    Add UserStatus enum and update UserStatusUpdate.cs
    - Added UserStatus enum in Mattermost.Models.Enums namespace.
    - Updated UserStatusUpdate.cs to include UserStatus property.
    - Added method to convert status text to UserStatus enum.
    - Marked UserStatus property with [JsonIgnore] attribute.

---
+++ Sources/Mattermost/Models/Enums/UserStatus.cs
@@ -0,0 +1,38 @@
+﻿namespace Mattermost.Models.Enums
+{
+    /// <summary>
+    /// User status.
+    /// </summary>
+    public enum UserStatus
+    {
+        /// <summary>
+        /// Custom status, ex. "Playing with Mattermost API".
+        /// </summary>
+        Custom = 0,
+
+        /// <summary>
+        /// Away status - user is not active.
+        /// </summary>
+        Away = 1,
+
+        /// <summary>
+        /// User is online and active.
+        /// </summary>
+        Online = 2,
+
+        /// <summary>
+        /// User is offline.
+        /// </summary>
+        Offline = 3,
+
+        /// <summary>
+        /// Do not disturb status.
+        /// </summary>
+        DoNotDisturb = 4,
+
+        /// <summary>
+        /// Custom status: "In a meeting"
+        /// </summary>
+        InMeeting = 5,
+    }
+}
