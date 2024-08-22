fb03ed929f6917e2c83e3af7eaa93f1fa0c5d792
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2024-08-21 20:02:15 -0700

    Refactor Routes class for maintainable API versioning
    Refactored the `Routes` class in the `Mattermost.Constants` namespace to use a private constant `version` for API endpoints, replacing hardcoded versions. Adjusted the order of some constants and moved `DefaultBaseUrl` to the end of the class. This centralizes the API version, improving maintainability and readability.

--- Sources/Mattermost/Constants/Routes.cs
+++ Sources/Mattermost/Constants/Routes.cs
@@ -2,14 +2,15 @@
 {
     internal static class Routes
     {
-        internal const string DefaultBaseUrl = "https://community.mattermost.com";
-        internal const string Users = "/api/v4/users";
-        internal const string WebSocket = "api/v4/websocket";
-        internal const string Posts = "/api/v4/posts";
-        internal const string Teams = "/api/v4/teams";
-        internal const string Files = "/api/v4/files";
+        private const string version = "/api/v4";
         internal const string Plugins = "/plugins";
-        internal const string Channels = "/api/v4/channels";
+        internal const string Users = version + "/users";
+        internal const string Posts = version + "/posts";
+        internal const string Teams = version + "/teams";
+        internal const string Files = version + "/files";
+        internal const string Channels = version + "/channels";
+        internal const string WebSocket = version + "/websocket";
         internal const string GroupChannels = Channels + "/group";
+        internal const string DefaultBaseUrl = "https://community.mattermost.com";
     }
 }
