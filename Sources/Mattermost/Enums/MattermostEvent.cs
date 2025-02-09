51f136d5d6860e9d4c02c7b627feea7820301a44
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2025-02-08 18:49:09 -0800

    Add XML documentation to MattermostEvent enum
    Added XML documentation comments to the MattermostEvent enum and its members in MattermostEvent.cs. This includes a summary for the enum itself and each member, explaining the event types they represent. Removed the #pragma warning disable CS1591 directive to enable warnings for missing XML comments.

--- Sources/Mattermost/Enums/MattermostEvent.cs
+++ Sources/Mattermost/Enums/MattermostEvent.cs
@@ -1,18 +1,58 @@
-﻿#pragma warning disable CS1591
-
-namespace Mattermost.Enums
+﻿namespace Mattermost.Enums
 {
+    /// <summary>
+    /// Event types which can be received from Mattermost server through WebSocket.
+    /// </summary>
     public enum MattermostEvent
     {
+        /// <summary>
+        /// Unknown event type (not defined in this enum).
+        /// </summary>
         Unknown = 0,
+
+        /// <summary>
+        /// Called when a new post is created.
+        /// </summary>
         Posted = 1,
+
+        /// <summary>
+        /// Called when user status changed.
+        /// </summary>
         StatusChange = 2,
+
+        /// <summary>
+        /// Called when user is typing text.
+        /// </summary>
         Typing = 3,
+
+        /// <summary>
+        /// Called when user viewed multiple channels.
+        /// </summary>
         MultipleChannelsViewed = 4,
+
+        /// <summary>
+        /// Called when user preferences changed.
+        /// </summary>
         PreferencesChanged = 5,
+
+        /// <summary>
+        /// Called when sidebar category updated.
+        /// </summary>
         SidebarCategoryUpdated = 6,
+
+        /// <summary>
+        /// Called when user added to channel.
+        /// </summary>
         UserAdded = 7,
+
+        /// <summary>
+        /// Called when ephemeral message received, e.g. when post is edited.
+        /// </summary>
         EphemeralMessage = 8,
+
+        /// <summary>
+        /// Called when user updated.
+        /// </summary>
         UserUpdated = 9,
     }
 }
